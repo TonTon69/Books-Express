1cd32914f0e6905e6a4f2a6dfecf6a72e152758a
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-11-08 16:40:03 +0700

    Query parameters

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    { id: 1, name: 'hoang'},
+    { id: 2, name: 'minh'},
+]
+
 app.get('/', function (req, res) {
     res.render('index')
 })
 app.get('/users', function (req, res) {
     res.render('users/index',{
-        users: [
-            { id: 1, name: 'hoang'},
-            { id: 2, name: 'minh'},
-        ]
+        users: users
+    })
+})
+app.get('/users/search', function (req, res) {
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('users/index',{
+        users: matchedUsers
     })
 })
 
