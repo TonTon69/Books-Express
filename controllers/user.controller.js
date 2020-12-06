132454921c73c53648afdc910e71313ffebfce04
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-12-06 18:37:47 +0700

    update signup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,49 +12,26 @@ module.exports.signup = (req, res) => {
   res.render("users/signup");
 };
 
-module.exports.postCreate = (req, res, next) => {
-  req.body.avatar = req.file.path;
-  cloudinary.uploader.upload(req.file.path, (err, result) => {
-    if (err) {
-      return res.redirect("/user/signup");
-    }
-    User.find({ email: req.body.email })
-      .exec()
-      .then((user) => {
-        if (user.length >= 1) {
-          res.render("users/signup", {
-            errors: ["This email already exists!"],
-            values: req.body,
-          });
-        } else {
-          bcrypt.hash(req.body.password, 10, (err, hash) => {
-            if (err) {
-              return res.status(500).json({
-                error: err,
-              });
-            } else {
-              const user = new User({
-                name: req.body.name,
-                email: req.body.email,
-                avatar: result.url,
-                password: hash,
-              });
-              user
-                .save()
-                .then(() => {
-                  res.render("auth/signin", {
-                    alerts: ["Sign Up Success!"],
-                  });
-                })
-                .catch((err) => {
-                  res.status(500).json({
-                    error: err,
-                  });
-                });
-            }
-          });
-        }
-      })
-      .catch(next);
-  });
+module.exports.postCreate = async (req, res) => {
+  const users = await User;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  if (req.file) {
+    req.body.avatar = req.file.path.split("/").slice(-2).join("/");
+    cloudinary.uploader.upload(req.file.path, (err, result) => {
+      if (err) {
+        return res.redirect("/user/signup");
+      } else {
+        const user = {
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+          avatar: result.url,
+        };
+        users.create(user);
+        res.render("auth/signin", {
+          alerts: ["Sign Up Success!"],
+        });
+      }
+    });
+  }
 };
--- middlewares/validates/user.validate.js
+++ middlewares/validates/user.validate.js
@@ -1,10 +1,15 @@
-module.exports = function (req, res, next) {
+const User = require("../../models/user.model");
+module.exports = async (req, res, next) => {
   var errors = [];
+  const checkEmail = await User.find({ email: req.body.email });
   if (!req.body.name) {
     errors.push("Please provide your name.");
   }
   if (!req.body.email) {
     errors.push("Please provide your email.");
+  }
+  if (checkEmail.length >= 1) {
+    errors.push("This email already exists.");
   }
   if (!req.body.password) {
     errors.push("Please provide your password.");
