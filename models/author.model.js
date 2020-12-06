5fb5a1c60f8c1885afc392524cfe606ab368567d
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-12-06 16:20:14 +0700

    view slug name author

--- models/author.model.js
+++ models/author.model.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
 const Schema = mongoose.Schema;
 
 const authorSchema = new Schema(
   {
     name: { type: String },
     image: { type: String },
+    slug: { type: String, slug: "name", unique: true },
   },
   {
     timestamps: true,
   }
 );
+mongoose.plugin(slug);
 module.exports = mongoose.model("Author", authorSchema);
